c922c03c9d1ad900042dced01d378d5fe570cfa9
Can <24660563+CanRaffs89@users.noreply.github.com>
2019-01-11 21:28:48 +0000

    Added image carousel

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Mobile menu dropdown toggle
 function classToggle() {
     const navbar = document.querySelectorAll('.nav-items')
     navbar.forEach(nav => nav.classList.toggle('nav-toggle-show'));
@@ -5,3 +6,22 @@ function classToggle() {
 
 document.querySelector('.nav-toggle')
     .addEventListener('click', classToggle);
+
+// Image carousel
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("slides");
+    if (n > x.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = x.length};
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex -1].style.display = "block";
+}
